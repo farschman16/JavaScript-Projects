47a2aeae3e287e667fafce1b1067ea970d4e03da
Farschman16 <farschman16@gmail.com>
2022-03-24 16:59:51 -0400

    JavaScript Basics
    First submission of JavaScript repository

---
+++ Basic JavaScript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,14 @@
+var A = "This is a string"; //first string
+document.write(A); //calling the string because it was assigned variable "A"
+document.write("Hello, World!"); //forcing the browser to print this text
+var B = "This is variable B. I am a string"; //
+window.alert(B);
+var C = "And then she said, \"I'm a computer, please stop acting like we\'re on a date. It\'s weird."; //utilizing backslash to use special characters without interrupting the code
+window.alert(C);
+var D = ("This is line 1 of this text string" //these are contatenated variables
++"This is the second line. How will it show up?"
++"And this is the third. See how this works");
+document.write(D);
+document.write(3+3); //expression within a statement
+var Doctors = "Modern Doctors", nine = "Christopher Eccleston", ten = "David Tennant", eleven = "Matt Smith", twelve = "Peter Capaldi", thirteen = "Jodie Whittaker";
+document.write(eleven.fontcolor('blue')); //this line utilized the deprecated "fontcolor" attribute
