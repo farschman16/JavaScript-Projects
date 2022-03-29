68fa1942aa4bececdb026cd4e918b32c3a854286
Farschman16 <farschman16@gmail.com>
2022-03-29 13:37:36 -0400

    Project3_math_operators

--- Basic JavaScript Projects/Project2_functions/JS/main.js
+++ Basic JavaScript Projects/Project2_functions/JS/main.js
@@ -8,3 +8,8 @@ function myFunction() { //function named 'myFunction' that displays the concaten
     sentence += " a lot from this book!";
     document.getElementById("Concatenate").innerHTML = sentence;
 }
+
+function multiply(a,b) {
+    var x = multiply(5, 6);
+    document.getElementById("demo").innerHTML = x;
+}
---
+++ Basic JavaScript Projects/Project3_math_operators/JS/main.js
@@ -0,0 +1,4 @@
+function addition_Function () {
+    var addition = 5 + 6;
+    document.getElementById("Math").innerHTML = "5 + 6 = " + addition;
+}
